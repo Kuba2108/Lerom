6c4bdbd980407e9209038b29ff7b5c9236cb12c7
Tolobaev Kubanychbek <kuba99426@gmail.com>
2024-04-23 17:46:51 +0600

    component end

--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Category from '../../components/Category/Category'
+import Presentation from '../../components/Presentation/Presentation'
 
 const HomePage = () => {
   return (
     <div>
-        
+        <Category/>
+        <Presentation/>
     </div>
   )
 }
