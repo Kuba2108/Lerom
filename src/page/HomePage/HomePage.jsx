f5cab1ca793fa2cfe19de5e55b97f1412304d2f6
Tolobaev Kubanychbek <kuba99426@gmail.com>
2024-04-23 18:22:53 +0600

    weds

--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -1,21 +1,15 @@
-import React from 'react'
-<<<<<<< HEAD
+
 import News from "../../components/News/News.jsx";
-import HomeNews from "../../components/News/HomeNews.jsx";
-=======
 import Category from '../../components/Category/Category'
 import Presentation from '../../components/Presentation/Presentation'
->>>>>>> 6c4bdbd980407e9209038b29ff7b5c9236cb12c7
+
 
 const HomePage = () => {
   return (
     <div>
-<<<<<<< HEAD
-        <News/>
-=======
-        <Category/>
+       <Category/>
         <Presentation/>
->>>>>>> 6c4bdbd980407e9209038b29ff7b5c9236cb12c7
+        <News/>
     </div>
   )
 }
